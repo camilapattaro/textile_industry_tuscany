d87e1c5ecbb046b1121ff8a5f82261e408c40342
Camila Pattaro <camilapattaro@hotmail.com>
2024-11-14 09:54:42 +0100

    Update Cleaning.sql

--- Cleaning.sql
+++ Cleaning.sql
@@ -1,6 +1,6 @@
 
 --1.My first step was checking for null values, filtering the columns one by one, on my SQL editor, DBeaver.
-----No null values were found.
+--No null values were found.
 
 --2.Finding duplicate values.
 
@@ -14,8 +14,8 @@ GROUP BY
 HAVING 
   COUNT (*) > 1
 
-----Two rows under the name INTERFIL - S.R.L. were found.
-----However, after checking those two rows, I could see they have different values, so I decide to keep them both in my analysis.
+--Two rows with the name INTERFIL - S.R.L. were identified.
+--However, upon reviewing these rows, I observed that they contain different values; therefore, I decided to retain both for my analysis."
 
 --3.Checking if there is any negative number for Investment values.
 
@@ -33,5 +33,6 @@ OR
   "Investimenti
 2015" < 1
 
-----No negative numbers where found.
+--No negative numbers where found.
+--The data is consistent and in the correct format. No further cleaning was necessary.
 
